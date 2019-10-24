ca24659d5bf4513e6fc11ea0efee458cd200a357
Student <example@example.com>
2019-10-24 13:46:57 -0700

    added if-else statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
 
-  $(".lion").click(function() {
-    $(".lion").slideToggle();
-  });
 
-  $(".giraffe").click(function() {
-    $(".giraffe").slideToggle();
-  });
 
-  $(".shark").click(function() {
-    $(".shark").slideToggle();
-  });
+var click = $(".panel-heading").click();
 
+
+if (click === lion) {
+  $(".giraffe").hide;
+  $(".shark").hide;
+
+} else if (click === giraffe) {
+  $(".lion").hide;
+  $(".shark").hide;
+} else if (click === shark) {
+  $(".giraffe").hide;
+  $(".lion").hide;
+} else {
+  console.log("yikes");
+}
+
+var lion = $(".lion").click(function() {
+  $(".lion").show();
+
+});
+
+var giraffe = $(".giraffe").click(function() {
+  $(".giraffe").show();
+});
+
+var shark = $(".shark").click(function() {
+  $(".shark").show();
+});
 
 });
